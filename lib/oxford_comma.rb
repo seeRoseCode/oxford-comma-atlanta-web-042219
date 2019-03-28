071d0279d384f95134940edf8c58b1d5aeba08d2
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-28 18:50:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+ if array.length == 0
+   array.join(",")
+ elsif array.length == 1
+   array.join
+ elsif array.length == 2
+   array.join(" and ")
+ else
+   array.length >= 3
+   new_string = array[0..-2].join (", ")
+   new_string << ", and #{array[-1]}"
+ end
 end
